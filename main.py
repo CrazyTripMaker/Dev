980b74fe5af6c4b7136765300b6b2d2966723b87
Darshil Jadeja <darshil.jadeja@proclink.local>
2026-01-05 16:17:31 +0530

    Add backend requirements

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="Tour Package Admin API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://dev-nfgp.onrender.com"],
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
